0ce685d3e0e0e5541663d9029b7fb3789c39fa6b
ihxnnxs <62433092+ihxnnxs@users.noreply.github.com>
2025-08-30 18:57:41 +0000

    Fix styling

--- src/UmbraUIServiceProvider.php
+++ src/UmbraUIServiceProvider.php
@@ -18,28 +18,28 @@ class UmbraUIServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'umbra-ui');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'umbra-ui');
 
         // Register anonymous components with prefix
-        Blade::anonymousComponentPath(__DIR__ . '/../resources/views/components', 'umbra-ui');
+        Blade::anonymousComponentPath(__DIR__.'/../resources/views/components', 'umbra-ui');
 
         // Register anonymous components without prefix
         if (config('umbra-ui.register_global', true)) {
-            Blade::anonymousComponentPath(__DIR__ . '/../resources/views/components');
+            Blade::anonymousComponentPath(__DIR__.'/../resources/views/components');
         }
 
         foreach ($this->components as $component) {
             $this->publishes([
-                __DIR__ . "/../resources/views/components/{$component}" => resource_path("views/components/{$component}"),
+                __DIR__."/../resources/views/components/{$component}" => resource_path("views/components/{$component}"),
             ], $component);
         }
 
         $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/umbra-ui'),
+            __DIR__.'/../resources/views' => resource_path('views/vendor/umbra-ui'),
         ], 'umbra-ui-views');
 
         $this->publishes([
-            __DIR__ . '/../config/umbra-ui.php' => config_path('umbra-ui.php'),
+            __DIR__.'/../config/umbra-ui.php' => config_path('umbra-ui.php'),
         ], 'umbra-ui-config');
     }
 }
