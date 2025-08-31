0a7f6ebf22945656d72da73ddd6fa23a84b219c3
ihxnnxs <crashermoney0@gmail.com>
2025-08-31 16:37:22 +0300

    added link + refactored components

--- resources/views/components/checkbox/index.blade.php
+++ resources/views/components/checkbox/index.blade.php
@@ -1,4 +1,1 @@
-<input {{ $attributes->merge([
-    'type' => 'checkbox',
-    'class' => 'w-5 h-5 border-2 border-neutral-300 rounded focus:outline-none focus:ring-2 focus:ring-neutral-600 focus:ring-offset-2 accent-neutral-900 transition-all duration-200 dark:border-neutral-800 dark:bg-neutral-950 dark:accent-neutral-50 dark:focus:ring-neutral-300'
-]) }} />
+<input {{ $attributes }} type="checkbox" class="w-5 h-5 border-2 border-neutral-300 rounded focus:outline-none focus:ring-2 focus:ring-neutral-600 focus:ring-offset-2 accent-neutral-900 transition-all duration-200 dark:border-neutral-800 dark:bg-neutral-950 dark:accent-neutral-50 dark:focus:ring-neutral-300" />
--- resources/views/components/input/index.blade.php
+++ resources/views/components/input/index.blade.php
@@ -1,16 +1,1 @@
-@php
-    $types = [
-        'text', 
-        'email', 
-        'password', 
-        'search', 
-        'tel', 
-        'url', 
-        'number'
-    ];
-    $type = $type ?? 'text';
-    $type = in_array($type, $types) ? $type : 'text';
-@endphp
-
-<input type="{{ $type }}"
-       {{ $attributes }} class="px-4 py-2 border-2 border-neutral-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-neutral-600 focus:ring-offset-2 focus:border-transparent transition-all duration-200 max-w-md w-full dark:bg-neutral-950 dark:border-neutral-800 dark:text-neutral-50 dark:placeholder-neutral-400 dark:focus:ring-neutral-300 dark:focus:border-neutral-300"/>
+<input {{ $attributes }} type="text" class="px-4 py-2 border-2 border-neutral-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-neutral-600 focus:ring-offset-2 focus:border-transparent transition-all duration-200 max-w-md w-full dark:bg-neutral-950 dark:border-neutral-800 dark:text-neutral-50 dark:placeholder-neutral-400 dark:focus:ring-neutral-300 dark:focus:border-neutral-300"/>
